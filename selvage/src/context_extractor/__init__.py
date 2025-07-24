f482ea5fc26ffdbe4b9a8ed680e61dc433b21919
anomie7 <anomie7777@gmail.com>
2025-07-24 10:56:46 +0900

    fix: ContextExtractor 클래스가 위치한 디렉토링명 변경
    Refs: CR-80

--- selvage/src/context_extractor/__init__.py
+++ selvage/src/context_extractor/__init__.py
@@ -1,8 +1,8 @@
 """최적화된 Tree-sitter 기반 컨텍스트 추출기 패키지."""
 
+from .context_extractor import ContextExtractor
 from .line_range import LineRange
 from .line_ranges_result import LineRangesResult
-from .optimized_context_extractor import ContextExtractor
 
 __all__ = [
     "LineRange",
